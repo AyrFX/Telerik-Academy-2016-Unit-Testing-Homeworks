d88e3f85f6dcfcfe53c79804b47f0b12b74d98f2
AyrFX <oleg@gbg.bg>
2016-07-21 13:20:57 +0300

    Created project for task 1 and class Student

---
+++ 01. Unit Testing/StudentsAndCourses/Student.cs
@@ -0,0 +1,54 @@
+﻿namespace StudentsAndCourses
+{
+	using System;
+	
+	public class Student
+	{
+		//Fields
+		private string name;
+		private int number;
+		
+		//Properties
+		public string Name {
+			get {
+				return this.name;
+			}
+			set
+			{
+				if (value != string.Empty)
+				{
+					this.name = value;
+				}
+				else
+				{
+					throw new ArgumentException("The name can not be empty!");
+				}
+			}
+		}
+		
+		public int Number
+		{
+			get
+			{
+				return this.number;
+			}
+			set
+			{
+				if (10000 <= value && value < 99999)
+				{
+					this.number = value;
+				}
+				else
+				{
+					throw new ArgumentException("The number should be between 10000 and 99999");
+				}
+			}
+		}
+		
+		//Constructors
+		public Student()
+		{
+						
+		}
+	}
+}
