4214f4567e3efcee9d1cb3cac6b44692e5ad6035
AyrFX <oleg@gbg.bg>
2016-07-25 07:26:42 +0300

    Added VSTT Test in Task 1 (according task description)

---
+++ 01. Unit Testing/SchoolDemo/School.Tests.VSTT/Course.Tests.cs
@@ -0,0 +1,60 @@
+﻿namespace School.Tests.VSTT
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using School;
+    using StudentsAndCourses;
+
+    [TestClass]
+    public class CourseTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.OverflowException))]
+        public void ToCouresCanNotBeJoinedMoreThan30Students()
+        {
+            var course = new Course();
+            for (int i = 0; i < 31; i++)
+            {
+                course.JoinStudent(new Student("A"));
+            }
+        }
+
+        [TestMethod]
+        public void StudentsListReturnRealJoinedStudentsNumber()
+        {
+            var course = new Course();
+            for (int i = 0; i < 20; i++)
+            {
+                course.JoinStudent(new Student("A"));
+            }
+            Assert.AreEqual(course.Students.Count, 20);
+        }
+
+        [TestMethod]
+        public void ReturnedStudentsListIsCorrect()
+        {
+            var course = new Course();
+            for (int i = 0; i < 10; i++)
+            {
+                course.JoinStudent(new Student("A"));
+            }
+
+            var studentsList = course.Students;
+            Assert.AreSame(course.Students, studentsList);
+        }
+
+        [TestMethod]
+        public void StudentCanLeaveCourse()
+        {
+            var course = new Course();
+            for (int i = 0; i < 10; i++)
+            {
+                course.JoinStudent(new Student("A"));
+            }
+
+            var studentToLeave = course.Students[4];
+            course.LeaveStudent(studentToLeave);
+            Assert.AreEqual(course.Students.IndexOf(studentToLeave), -1);
+        }
+    }
+}
---
+++ 01. Unit Testing/SchoolDemo/School.Tests.VSTT/Student.Tests.cs
@@ -0,0 +1,56 @@
+﻿namespace School.Tests.VSTT
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using School;
+    using StudentsAndCourses;
+
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void StudentNameShouldNotBeEmpty()
+        {
+            var student = new Student("");
+        }
+
+        [TestMethod]
+        public void AssignedStudentNumberIncrementsCorrectly()
+        {
+            Student.InitializeNumber();
+            var firstStudent = new Student("ABCD");
+            var secondStudent = new Student("ABCD");
+            Assert.AreEqual(firstStudent.Number, secondStudent.Number - 1);
+        }
+
+        [TestMethod]
+        public void CreateStudentByValidData()
+        {
+            Student.InitializeNumber();
+            var student = new Student("A");
+            Assert.IsNotNull(student);
+        }
+
+        [TestMethod]
+        public void TheNameOfTheStudentEqualsToGivenStudentName()
+        {
+            Student.InitializeNumber();
+            var student = new Student("ABCD");
+            Assert.AreEqual(student.Name, "ABCD");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void AddingMoreThan89999StudentThrowsException()
+        {
+            Student.InitializeNumber();
+            Student student;
+            for (int i = 0; i < 89999; i++)
+            {
+                student = new Student("ABCD");
+            }
+           student = new Student("ABCD");
+        }
+    }
+}
