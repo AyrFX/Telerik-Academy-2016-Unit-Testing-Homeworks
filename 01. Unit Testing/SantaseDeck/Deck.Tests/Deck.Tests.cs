aaee2a7b60b3355346c3178baa676ec987c294e5
AyrFX <oleg@gbg.bg>
2016-07-25 21:58:35 +0300

    Task 2 of Unit testing homework: added parameterized test instead of two old tests

--- 01. Unit Testing/SantaseDeck/Deck.Tests/Deck.Tests.cs
+++ 01. Unit Testing/SantaseDeck/Deck.Tests/Deck.Tests.cs
@@ -7,129 +7,123 @@ using Santase.Logic.Cards;
 namespace Deck.Tests
 {
 	[TestFixture]
-    public class DeckTests
-    {
-        [Test]
-        public void TrumpCardShouldBeNonNullable()
+	public class DeckTests
+	{
+		[Test]
+		public void TrumpCardShouldBeNonNullable()
+		{
+			IDeck deck = new Santase.Logic.Cards.Deck();
+			
+			Assert.That(deck.TrumpCard, Is.Not.Null);
+		}
+
+		[Test]
+		public void TrumpCardShouldBeRandom()
+		{
+			const int NumberOfRandomDecks = 25;
+			var lastCard = new Santase.Logic.Cards.Deck().TrumpCard;
+			for (var i = 0; i < NumberOfRandomDecks - 1; i++)
+			{
+				IDeck deck = new Santase.Logic.Cards.Deck();
+				if (!deck.TrumpCard.Equals(lastCard))
+				{
+					return;
+				}
+
+				lastCard = deck.TrumpCard;
+			}
+
+			Assert.Fail(NumberOfRandomDecks + " times generated the same trump card!");
+		}
+
+		[Test]
+		public void CardsLeftShouldBe24ForANewDeck()
+		{
+			IDeck deck = new Santase.Logic.Cards.Deck();
+			
+			Assert.That(deck.CardsLeft, Is.EqualTo(24));
+		}
+
+        [TestCase(0,24)]
+        [TestCase(3,21)]
+        [TestCase(7,17)]
+        [TestCase(24,0)]
+        public void CardsLeftPlusCardsDrawnShouldBe24(int cardstoDraw, int left)
         {
             IDeck deck = new Santase.Logic.Cards.Deck();
-            
-            Assert.That(deck.TrumpCard, Is.Not.Null);
-        }
-
-        [Test]
-        public void TrumpCardShouldBeRandom()
-        {
-            const int NumberOfRandomDecks = 25;
-            var lastCard = new Santase.Logic.Cards.Deck().TrumpCard;
-            for (var i = 0; i < NumberOfRandomDecks - 1; i++)
-            {
-                IDeck deck = new Santase.Logic.Cards.Deck();
-                if (!deck.TrumpCard.Equals(lastCard))
-                {
-                    return;
-                }
-
-                lastCard = deck.TrumpCard;
-            }
-
-            Assert.Fail(NumberOfRandomDecks + " times generated the same trump card!");
-        }
-
-        [Test]
-        public void CardsLeftShouldBe24ForANewDeck()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            
-            Assert.That(deck.CardsLeft, Is.EqualTo(24));
-        }
-
-        [Test]
-        public void CardsLeftShouldBe23AfterDrawingOneCard()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            deck.GetNextCard();
-            
-            Assert.That(deck.CardsLeft, Is.EqualTo(23));
-        }
-
-        [Test]
-        public void CardsLeftShouldBe0AfterDrawing24Cards()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            for (var i = 0; i < 24; i++)
-            {
-                deck.GetNextCard();
-            }
-            
-            Assert.That(deck.CardsLeft, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void GetNextCardShouldThrowExceptionWhenCalled25Times()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            for (var i = 0; i < 24; i++)
+            for (int i = 0; i < cardstoDraw; i++)
             {
                 deck.GetNextCard();
             }
 
-            Assert.That(() => { deck.GetNextCard(); }, Throws.Exception.TypeOf<InternalGameException>());
+            Assert.That(deck.CardsLeft, Is.EqualTo(left));
         }
+        		
+		[Test]
+		public void GetNextCardShouldThrowExceptionWhenCalled25Times()
+		{
+			IDeck deck = new Santase.Logic.Cards.Deck();
+			for (var i = 0; i < 24; i++)
+			{
+				deck.GetNextCard();
+			}
 
-        [Test]
-        public void GetNextCardShouldNotChangeTheTrumpCard()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            var trumpBefore = deck.TrumpCard;
-            deck.GetNextCard();
-            var trumpAfter = deck.TrumpCard;
-            
-            Assert.That(trumpBefore, Is.EqualTo(trumpAfter));
-        }
+			Assert.That(() => { deck.GetNextCard(); }, Throws.Exception.TypeOf<InternalGameException>());
+		}
 
-        [Test]
-        public void GetNextCardShouldReturnDifferentNonNullCardEveryTime()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            var cards = new HashSet<Card>();
-            var cardsCount = deck.CardsLeft;
-            for (var i = 0; i < cardsCount; i++)
-            {
-                var card = deck.GetNextCard();
-                
-                Assert.That(card, Is.Not.Null);
-                Assert.That(cards.Contains(card), Is.False);
-                cards.Add(card);
-            }
-        }
+		[Test]
+		public void GetNextCardShouldNotChangeTheTrumpCard()
+		{
+			IDeck deck = new Santase.Logic.Cards.Deck();
+			var trumpBefore = deck.TrumpCard;
+			deck.GetNextCard();
+			var trumpAfter = deck.TrumpCard;
+			
+			Assert.That(trumpBefore, Is.EqualTo(trumpAfter));
+		}
 
-        [Test]
-        public void ChangeTrumpCardShouldWorkProperly()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            var card = new Card(CardSuit.Spade, CardType.Nine);
-            deck.ChangeTrumpCard(card);
-            var trumpCard = deck.TrumpCard;
-            
-            Assert.That(card, Is.EqualTo(trumpCard));
-        }
+		[Test]
+		public void GetNextCardShouldReturnDifferentNonNullCardEveryTime()
+		{
+			IDeck deck = new Santase.Logic.Cards.Deck();
+			var cards = new HashSet<Card>();
+			var cardsCount = deck.CardsLeft;
+			for (var i = 0; i < cardsCount; i++)
+			{
+				var card = deck.GetNextCard();
+				
+				Assert.That(card, Is.Not.Null);
+				Assert.That(cards.Contains(card), Is.False);
+				cards.Add(card);
+			}
+		}
 
-        [Test]
-        public void ChangeTrumpCardShouldChangeTheLastCardInTheDeck()
-        {
-            IDeck deck = new Santase.Logic.Cards.Deck();
-            var card = new Card(CardSuit.Club, CardType.Ace);
-            deck.ChangeTrumpCard(card);
-            var cardsCount = deck.CardsLeft;
-            for (var i = 0; i < cardsCount - 1; i++)
-            {
-                deck.GetNextCard();
-            }
+		[Test]
+		public void ChangeTrumpCardShouldWorkProperly()
+		{
+			IDeck deck = new Santase.Logic.Cards.Deck();
+			var card = new Card(CardSuit.Spade, CardType.Nine);
+			deck.ChangeTrumpCard(card);
+			var trumpCard = deck.TrumpCard;
+			
+			Assert.That(card, Is.EqualTo(trumpCard));
+		}
 
-            var lastCard = deck.GetNextCard();
-            
-            Assert.That(card, Is.EqualTo(lastCard));
-        }
+		[Test]
+		public void ChangeTrumpCardShouldChangeTheLastCardInTheDeck()
+		{
+			IDeck deck = new Santase.Logic.Cards.Deck();
+			var card = new Card(CardSuit.Club, CardType.Ace);
+			deck.ChangeTrumpCard(card);
+			var cardsCount = deck.CardsLeft;
+			for (var i = 0; i < cardsCount - 1; i++)
+			{
+				deck.GetNextCard();
+			}
+
+			var lastCard = deck.GetNextCard();
+			
+			Assert.That(card, Is.EqualTo(lastCard));
+		}
 	}
 }
