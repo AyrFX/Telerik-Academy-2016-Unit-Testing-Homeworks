5b553df5b51587e5d6896126fb85ec1915961bd8
AyrFX <oleg@gbg.bg>
2016-07-28 22:54:02 +0300

    Added Mocking homework task with some test written.

---
+++ 03. Mocking and JustMock/Cars.Tests.Moq/CarsRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+using Cars.Contracts;
+using Cars.Data;
+using Cars.Models;
+
+namespace Cars.Tests.Moq
+{
+    [TestClass]
+    public class CarsRepositoryTests
+    {
+        [TestMethod]
+        public void ConstructorWithParameterShouldCreateObjectsProperly()
+        {
+            //Arrange
+            var mockedDatabase = new Mock<IDatabase>();
+
+            //Act
+            var repository = new CarsRepository(mockedDatabase.Object);
+
+            //Assert
+            Mock.Verify(mockedDatabase);
+        }
+
+        [TestMethod]
+        public void ConstructorWithoutParameterShouldCreateObjectsProperly()
+        {
+            //Arrange
+            var mockedCarsRepository = new Mock<ICarsRepository>();
+
+            //Act
+            mockedCarsRepository.Setup(x => x.Add(It.IsAny<Car>()));
+
+            //Assert
+            mockedCarsRepository.Setup(x => x.All());
+        }
+    }
+}
